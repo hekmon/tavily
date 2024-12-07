ce731bc9371614972f4af503b4b1fddf6c985e20
Hekmon <3418467+hekmon@users.noreply.github.com>
2024-12-07 21:36:09 +0100

    search done

--- search.go
+++ search.go
@@ -2,8 +2,11 @@ package tavily
 
 import (
 	"context"
+	"encoding/json"
 	"errors"
 	"fmt"
+	"net/url"
+	"time"
 )
 
 // SearchQuery represents the parameters for a search query.
@@ -76,20 +79,128 @@ type searchQueryAuth struct {
 	SearchQuery
 }
 
-func (c *Client) Search(ctx context.Context, query SearchQuery) (err error) {
+func (c *Client) Search(ctx context.Context, query SearchQuery) (answer SearchAnswer, err error) {
 	// Prepare query
-	if err := query.Validate(); err != nil {
-		return fmt.Errorf("failed to validate search query: %w", err)
+	if err = query.Validate(); err != nil {
+		err = fmt.Errorf("failed to validate search query: %w", err)
+		return
 	}
 	authedQuery := searchQueryAuth{
 		APIKey:      c.apiKey,
 		SearchQuery: query,
 	}
 	// Execute
-	var output any
-	if err = c.request(ctx, "search", authedQuery, &output); err != nil {
-		return fmt.Errorf("failed to execute API query: %w", err)
+	if err = c.request(ctx, "search", authedQuery, &answer); err != nil {
+		err = fmt.Errorf("failed to execute API query: %w", err)
 	}
-	fmt.Printf("%+v\n", output)
 	return
+}
+
+// SearchAnswer represents the response from the search API.
+// https://docs.tavily.com/docs/rest-api/api-reference#response
+type SearchAnswer struct {
+	Query             string               `json:"query"`
+	FollowUpQuestions []string             `json:"follow_up_questions"`
+	Answer            *string              `json:"answer"`
+	Images            []SearchAnswerImage  `json:"images"`
+	Results           []SearchAnswerResult `json:"results"`
+	ResponseTime      time.Duration        `json:"-"`
+}
+
+func (sa *SearchAnswer) UnmarshalJSON(data []byte) (err error) {
+	type mask SearchAnswer
+	tmp := struct {
+		*mask
+		ResponseTime float64 `json:"response_time"`
+	}{
+		mask: (*mask)(sa),
+	}
+	if err = json.Unmarshal(data, &tmp); err != nil {
+		return fmt.Errorf("failed to unmarshal JSON into tmp struct: %w", err)
+	}
+	sa.ResponseTime = time.Duration(tmp.ResponseTime) * time.Second
+	return
+}
+
+func (sa *SearchAnswer) MarshalJSON() ([]byte, error) {
+	type mask SearchAnswer
+	tmp := struct {
+		*mask
+		ResponseTime float64 `json:"response_time"`
+	}{
+		mask:         (*mask)(sa),
+		ResponseTime: sa.ResponseTime.Seconds(),
+	}
+	return json.Marshal(tmp)
+}
+
+type SearchAnswerImage struct {
+	URL         *url.URL `json:"-"`
+	Description string   `json:"description"`
+}
+
+func (sai *SearchAnswerImage) UnmarshalJSON(data []byte) (err error) {
+	type mask SearchAnswerImage
+	tmp := struct {
+		URL string `json:"url"`
+		*mask
+	}{
+		mask: (*mask)(sai),
+	}
+	if err = json.Unmarshal(data, &tmp); err != nil {
+		return fmt.Errorf("failed to unmarshal JSON into tmp struct: %w", err)
+	}
+	if sai.URL, err = url.Parse(tmp.URL); err != nil {
+		return fmt.Errorf("failed to parse URL: %w", err)
+	}
+	return
+}
+
+func (sai *SearchAnswerImage) MarshalJSON() ([]byte, error) {
+	type mask SearchAnswerImage
+	tmp := struct {
+		URL string `json:"url"`
+		*mask
+	}{
+		URL:  sai.URL.String(),
+		mask: (*mask)(sai),
+	}
+	return json.Marshal(tmp)
+}
+
+type SearchAnswerResult struct {
+	Title      string   `json:"title"`
+	URL        *url.URL `json:"-"`
+	Content    string   `json:"content"`
+	Score      float64  `json:"score"`
+	RawContent *string  `json:"raw_content"`
+}
+
+func (sar *SearchAnswerResult) UnmarshalJSON(data []byte) (err error) {
+	type mask SearchAnswerResult
+	tmp := struct {
+		*mask
+		URL string `json:"url"`
+	}{
+		mask: (*mask)(sar),
+	}
+	if err = json.Unmarshal(data, &tmp); err != nil {
+		return fmt.Errorf("failed to unmarshal JSON into tmp struct: %w", err)
+	}
+	if sar.URL, err = url.Parse(tmp.URL); err != nil {
+		return fmt.Errorf("failed to parse URL: %w", err)
+	}
+	return
+}
+
+func (sar *SearchAnswerResult) MarshalJSON() ([]byte, error) {
+	type mask SearchAnswerResult
+	tmp := struct {
+		URL string `json:"url"`
+		*mask
+	}{
+		URL:  sar.URL.String(),
+		mask: (*mask)(sar),
+	}
+	return json.Marshal(tmp)
 }
